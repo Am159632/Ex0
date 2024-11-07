90d1533595235712abedbf8a66a7cbaea3936f7b
Am159632 <asaf.margaliot@gmail.com>
2024-11-07 20:30:25 +0200

    E0x

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,52 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
+
+import static java.lang.System.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args)
-    {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
         //a
-        System.out.println("Enter a natural even number (>4):");
-        int n= scanner.nextInt();
-        int p1=0,p2=0;
-        for (int i=3;i<=n/2;i++)
+        out.println("Enter a natural even number (>4):");
+        int n = scanner.nextInt();
+        int p1 = 0, p2 = 0;
+        boolean e1 = true,e2=true;
+        for (int i = 3; i <= n / 2; i++)
         {
-            p1=i;
-            p2=n-i;
+            for (int j = 2; j < i; j++)
+
+                if (i % j == 0)
+                {
+                    e1 = false;
+                }
+            if (e1 == true)
+            {
+                for (int k = 2; k < n - i; k++)
+                {
+                    if (n - i % k == 0)
+                    {
+                        e2 = false;
+                    }
+                }
+                if (e2 == true)
+                {
+                    p1 = i;
+                    p2 = n - i;
+                }
+            }
+        }
+        System.out.println(n+"="+p1+"+"+p2);
+        //end a
+        //b
+       boolean e3=true,e4=true;
+        int p3=0,p4=0;
+        while(e3==true&&e4==true)
+        {
 
         }
     }
-}
+    }
+
+
+
