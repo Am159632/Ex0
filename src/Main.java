db53ee82b276fdffc1ee04ec82eda2e8a6ae8145
Am159632 <asaf.margaliot@gmail.com>
2024-11-10 15:14:28 +0200

    E0x

--- src/Main.java
+++ src/Main.java
@@ -8,94 +8,62 @@ import static java.lang.System.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static boolean Isprime( int num)
+    {
+        boolean e = true;
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                e = false;
+            }
+        }
+        return e;
+    }
     public static void main(String[] args)
     {
-        public int Isprime( int num)
-        {
-            int e = 0;
-            for (int i = 2; i < num; i++)
-            {
-                if (num % i == 0)
-                {
-                    e = 1;
-                }
-            }
-            return e;
-        }
+
         Scanner scanner = new Scanner(in);
         //a
         out.println("Enter a natural even number (>4):");
         int n = scanner.nextInt();
         int p1 = 0, p2 = 0;
-        boolean e1 = true, e2 = true;
-        for (int i = 3; i <= n / 2; i++) {
-            for (int j = 2; j < i; j++)
-
-                if (i % j == 0) {
-                    e1 = false;
-                }
-            for (int k = 2; k < n - i; k++) {
-                if (n - i % k == 0) {
-                    e2 = false;
-                }
-            }
-            if (e1 == true && e2 == true) {
-                p1 = i;
-                p2 = n - i;
+        for (int i = 3; i <n / 2; i++)
+        {
+            if (Isprime(i)==true&&Isprime(n-i)==true)
+            {
+                p1=i;
+                p2=n-i;
+                break;
             }
         }
         System.out.println(n+"="+p1+"+"+p2);
         //end a
         //b
-       boolean e3=true,e4=true;
         int p3=0,p4=0,index=2,sign=0;
         while(sign==0)
         {
-            for (int i=2;i<n+index;i++)
-            {
-                if (n+index% i == 0)
-                {
-                    e3 = false;
-                }
-            }
-            for (int k =2 ; k <index; k++)
-            {
-                if ((n+index)% k == 0)
-                {
-                    e3 = false;
-                }
-            }
-            if(e3==true&&e4==true)
+            if(Isprime(n+index)==true&&Isprime(index)==true)
             {
                 p3=n+index;
                 p4=index;
                 System.out.println(n+"="+p3+"-"+p4);
-                sign=1;
+                break;
             }
             index++;
         }
         //end b
         //c
         int[]a=new int[n];
-        index=0;
-        boolean e5;
+       int count=0;
         for (int i=2;i<n;i++)
         {
-            e5=true;
-            for (int k=2;k<i;k++)
+            if (Isprime(n)==true)
             {
-                if (i%k==0)
-                {
-                    e5=false;
-                }
-            }
-            if (e5==true)
-            {
-                a[index]=i;
-                index++;
+                a[count]=i;
+                count++;
             }
         }
+       System. out.println(count+" prime numbers in [2,"+n+")");
     }
 }
-
-
