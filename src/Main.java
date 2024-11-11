cc42bbce4376d2b5532b9103c5ac91414cdaaaa2
Am159632 <asaf.margaliot@gmail.com>
2024-11-11 15:07:24 +0200

    E0x

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import javax.sound.midi.SysexMessage;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.concurrent.LinkedTransferQueue;
 import static java.lang.System.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -11,31 +7,30 @@ public class Main {
     //Action that reciving integer number and return true if prime,false if not
     static boolean Isprime( int num)
     {
-        boolean e = true;
-        for (int i = 2; i <=num/2; i++)//Passing on the numbers from 2 to num/2 because after this
+        for (int i = 2; i <= Math.sqrt(num); i++)//Passing on the numbers from 2 to num/2 because after this
           //  i don't need to check(2 is the smallest number)/I check if the number is divided by all numbers
             // between 2 to num/2 without rest.If this correct,return true,else,return false.
             {
             if (num % i == 0)
             {
-                e = false;
+                return false;
             }
         }
-        return e;
+        return true;
     }
     public static void main(String[] args)
     {
-        long startTime = System.nanoTime();//Start time
+        long startTime = nanoTime();//Start time
         Scanner scanner = new Scanner(in);
-        out.println("Enter a natural even number (>4):");//Input even number bigger than 4
+        System.out.println("Enter a natural even number (>4):");//Input even number bigger than 4
+        int n = scanner.nextInt();
         //a
         //Searching pair of numbers that p1+p2=n that p1,p2 are prime numbers.
-        int n = scanner.nextInt();
         int p1 = 0, p2 = 0;
         for (int i = 2; i <n ; i++)//passing on the numbers from 2 because 1 is not prime to n-1
        // because n is not prime(even and divided by 2)
         {
-            if ((Isprime(i) == true) && (Isprime(n - i) == true))//check if i and his couple(n-i) are prime.if
+            if ((Isprime(i)) && (Isprime(n - i)))//check if i and his couple(n-i) are prime.if
 
             {//If right,put the pair of the numbers in p1,p2 and end the loop
                 p1=i;
@@ -46,48 +41,40 @@ public class Main {
         System.out.println(n+"="+p1+"+"+p2);//print the numbers(example:20=3+17
         //end a
         //b
-        int p3=0,p4=0,index=2,sign=0;
-        while(sign==0)
+        //Searching pair of numbers that maintains n=p3-p4 and print then.(p3,p4 are prime numbers)
+        int p4,p3;
+        for(int i = 2; i <= n * 100; i++)
         {
-            if(Isprime(n+index)==true&&Isprime(index)==true)
+            if(Isprime(n+i) && Isprime(i))
             {
-                p3=n+index;
-                p4=index;
+                p3=n;
+                p4 = n - i;
                 System.out.println(n+"="+p3+"-"+p4);
                 break;
             }
-            index++;
         }
         //end b
         //c
-        int[] a=new int[n];
-        int count=0;
-        for (int i=2;i<n;i++)
+        int count = 0;
+        for (int i = 2; i < n; i++)
         {
-
-            if (Isprime(i)==true)
+            out.println(i);
+            if (Isprime(i))
             {
-                a[count]=i;
                 count++;
             }
         }
-        System. out.print(count+" prime numbers in [2,"+n+"):");
-        System. out.print("//("+a[0]);
-        for(int i=1;i<count;i++)
-        {
-            System. out.print(","+a[i]);
-        }
-        System. out.println(")");
+        System.out.println(count+" prime numbers in [2,"+n+")");
         //end c
         //d
-       index=2;
+        int index=2;
         int n1=n/2;
-        System. out.print(n+"="+index);
+        System.out.print(n+"="+index);
         while(n1>1)
         {
             if (Isprime(index)==true&&n1%index==0)
             {
-                System. out.print("*"+index);
+                System.out.print("*"+index);
                 n1=n1/index;
             }
             else
@@ -95,19 +82,12 @@ public class Main {
                 index++;
             }
         }
-        System. out.println("");
+        System.out.println("");
         //end d
         //e
-        System. out.println("//solely designed and implemented by 207302456");
-        long endTime = System.nanoTime();
-        double duration = (endTime - startTime)/1000000000.0;//nanoseconds
-        System.out.println("//the program runtime "+duration+" seconds");
+        System.out.println("207302456");
+        long endTime = nanoTime();
+        double duration = (endTime - startTime)/1_000_000_000.0;//seconds
+        System.out.println(duration+" seconds");
     }
 }
-/*
-חיבור github
-שינוי גודל מערך
-האם צריך את ההוספות
-איך הופכים לשניות את הזמן הרצה
-איך גורמים לתכנית לרוץ במספר גדול
- */
