3b7e0f62b052a18f4b0dd3fef4da4e7b8286ecad
Am159632 <asaf.margaliot@gmail.com>
2024-11-14 09:30:27 +0200

    E0x

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,6 @@ import static java.lang.System.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    /*FUNCTION IsPrime(num)
-        FOR i = 2 TO square root of num
-            IF num MOD i == 0 THEN
-                RETURN false
-            END IF
-        END FOR
-
-        RETURN true
-    END FUNCTION*/
     static boolean Isprime(int num)
     {
         for (int i = 2; i <= Math.sqrt(num); i++)
@@ -31,26 +22,11 @@ public class Main {
             System.out.println("error");
             return;
         }
-        /*a
-        START
-        FUNCTION findPrimeSum(n)
-            p1 = 0
-            p2 = 0
-
-            FOR i = 2 TO (n-1)
-               IF isPrime(i) AND isPrime(n - i) THEN
-                    p1 = i
-                    p2 = n - i
-                    BREAK
-               END IF
-                END FOR
-        PRINT n + " = " + p1 + " + " + p2
-        END FUNCTION*/
+        //a
         int p1=0,p2=0;
-        for (int i = 2; i < n; i++)//passing on the numbers from 2 because 1 is not prime to n-1
-        // because n is not prime(even and divided by 2)
+        for (int i = 2; i < n; i++)
         {
-            if ((Isprime(i)) && (Isprime(n - i)))//check if i and his couple(n-i) are prime.if
+            if ((Isprime(i)) && (Isprime(n - i)))
             {
                 p1 = i;
                 p2 = n - i;
@@ -58,35 +34,7 @@ public class Main {
             }
         }
         System.out.println(n + "=" + p1 + "+" + p2);
-        /*b (68759678)
-        Searching pair of numbers that maintains n=p3-p4 and print then.(p3,p4 are prime numbers)
-        START
-        DECLARE p3, p4
-
-    FOR i = 2 TO (n * 100)
-        IF isPrime(n + i) AND isPrime(i) THEN
-            p3 = n + i
-            p4 = i
-            PRINT n + " = " + p3 + " - " + p4
-            BREAK
-        END IF
-    END FOR
-END FUNCTION
-
-FUNCTION isPrime(num)
-    IF num <= 1 THEN
-        RETURN false
-    END IF
-
-    FOR i = 2 TO square root of num
-        IF num MOD i == 0 THEN
-            RETURN false
-        END IF
-    END FOR
-
-    RETURN true
-END FUNCTION
-         */
+        //b
         int p4, p3;
         for (int i = 2; i < 100_000_000; i++) {
             if (Isprime(n + i) && Isprime(i)) {
@@ -96,20 +44,7 @@ END FUNCTION
                 break;
             }
         }
-        /*c
-        START
-
-FUNCTION countPrimes(n)
-    DECLARE count = 0
-
-    FOR i = 2 TO (n - 1)
-        IF isPrime(i) THEN
-            count = count + 1
-        END IF
-    END FOR
-
-    PRINT count + " prime numbers
-        */
+        //c
         int count = 1;
         for (int i = 3; i < n; i+=2)
 
@@ -117,39 +52,7 @@ FUNCTION countPrimes(n)
                 count++;
 
         System.out.println(count + " prime numbers in [2," + n + ")");
-        /*d
-        START
-
-    DECLARE index = 2
-    DECLARE n1 = n / 2
-    PRINT n + " = 2"
-
-    WHILE n1 > 1
-        IF isPrime(index) AND n1 MOD index == 0 THEN
-            PRINT " * " + index
-            n1 = n1 / index
-        ELSE
-            index = index + 1
-        END IF
-    END WHILE
-
-    PRINT newline
-END FUNCTION
-
-FUNCTION isPrime(num)
-    IF num <= 1 THEN
-        RETURN false
-    END IF
-
-    FOR i = 2 TO square root of num
-        IF num MOD i == 0 THEN
-            RETURN false
-        END IF
-    END FOR
-
-    RETURN true
-END FUNCTION
-         */
+        //d
         int index = 2;
         System.out.print(n + "=" +2);
         n=n/2;
@@ -160,24 +63,10 @@ END FUNCTION
             } else
                 index++;
 
-        /*e
-        PRINT "207302456"(my id)
-         */
+        //e
         System.out.println();
         System.out.println("207302456");
-        /*f
-        START
-
-DECLARE end AS long
-SET start TO start time in nanoseconds(line 29)
-SET end TO current time in nanoseconds
-
-DECLARE duration AS double
-SET duration TO (end - start) / 1_000_000_000.0  // Convert nanoseconds to seconds
-
-PRINT duration + " seconds"
-END FUNCTION
-         */
+        //f
         long end = nanoTime();
         double duration = (end - start) / 1_000_000_000.0;//seconds
         System.out.println(duration + " seconds");
